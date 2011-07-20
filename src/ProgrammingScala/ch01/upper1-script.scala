86820a52080e4b66ed59e0e6faeba49dcfedfab9
Daniel Cheah <me@dancheah.com>
2011-07-19 20:50:17 -0700

    worked through 1st chapter

---
+++ src/ProgrammingScala/ch01/upper1-script.scala
@@ -0,0 +1,10 @@
+class Upper {
+    def upper(strings: String*): Seq[String] = {
+        strings.map((s:String) => s.toUpperCase())
+    }
+}
+
+val up = new Upper
+Console.println(up.upper("A", "First", "Scala", "Program"))
+
+// vim: set ts=4 sw=4 et
