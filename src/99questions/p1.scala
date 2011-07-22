5285b071252acf5cb07eba2190e0e202c51e228a
Daniel Cheah <me@dancheah.com>
2011-07-21 22:16:29 -0700

    some solutions to the 99 scala questions.

---
+++ src/99questions/p1.scala
@@ -0,0 +1,11 @@
+object P1 {
+    def last(l: List[Int]): Int = {
+        if (l.tail == List()) {
+            l.head
+        } else {
+            last(l.tail)
+        }
+    }
+}
+
+// vim: set ts=4 sw=4 et:
---
+++ src/99questions/p4.scala
@@ -0,0 +1,13 @@
+object P4 {
+    // This is a very lispish style
+    def length(l: List[Any]): Int = {
+        if (l == List()) {
+            0 
+        } else {
+            1 + length(l.tail)
+        }
+    }
+}
+
+
+// vim: set ts=4 sw=4 et:
