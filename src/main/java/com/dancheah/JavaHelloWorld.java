c8fd95ccf15e7845da5c4d0b2da8ac8ace6912ed
Daniel Cheah <me@dancheah.com>
2011-08-19 21:46:31 -0700

    Added some java placeholders

---
+++ src/main/java/com/dancheah/JavaHelloWorld.java
@@ -0,0 +1,10 @@
+package com.dancheah;
+
+public class JavaHelloWorld {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/com/dancheah/JavaTest.java
@@ -0,0 +1,11 @@
+package com.dancheah;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class JavaTest {
+    @Test
+    public void testTrue() {
+        assertTrue(true);
+    }
+}
